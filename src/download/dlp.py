dd64259f5a354099481dd878d04cdef3d0953822
levavft-eam <levavft@ethan-alice.com>
2024-04-22 17:20:15 +0900

    Add dlp.py and consequences.

---
+++ src/download/dlp.py
@@ -0,0 +1,44 @@
+from yt_dlp import YoutubeDL
+import os
+
+BASE_DIR = "../data"
+
+
+final_filename = None
+
+
+def _yt_dlp_monitor(d):
+    global final_filename
+    final_filename = d.get('info_dict').get('_filename')
+
+
+def download_video(video_id):
+    video_url = f"https://www.youtube.com/watch?v={video_id}"
+
+    ydl_opts = {
+        # 'format': 'best[height<=720]',
+        'format': 'bestaudio',
+        'outtmpl': f'{BASE_DIR}/%(id)s.%(ext)s',
+        # 'download_archive': 'download_archive.txt',
+        'progress_hooks': [_yt_dlp_monitor]
+        # 'postprocessors': [{
+        #     'key': 'ExecAfterDownload',  # Execute a command after download
+        #     'exec_cmd': 'ffmpeg -i {} -ss 00:00:00 -t 30 -c copy {}_trimmed.mp4',  # Trim the first 30 seconds
+        # }],
+
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download([video_url])
+    return final_filename
+
+
+def list_formats(video_id):
+    video_url = f"https://www.youtube.com/watch?v={video_id}"
+
+    ydl_opts = {
+        'listformats': True
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        result = ydl.download([video_url])
+    return result
--- src/main.py
+++ src/main.py
@@ -1,13 +1,24 @@
 import logging
 
 from common.setup_logging import setup_logging
-from stt.whisper import load_pipeline
+# from stt.whisper import load_pipeline
+from download.dlp import list_formats, download_video
+
+
+logger = logging.getLogger(__name__)
+setup_logging()
+
+
+def main():
+    video_id = "c8OwVTBdE6s"
+
+    logger.info("Program started")
+    # pipeline = load_pipeline()
+    # logger.info(list_formats(video_id))
+    logger.info(download_video(video_id))
+    logger.info("Program finished")
 
 
 if __name__ == "__main__":
-    setup_logging()
-    logger = logging.getLogger(__name__)
+    main()
 
-    logger.info("Program started")
-    pipeline = load_pipeline()
-    logger.info("Program finished")
