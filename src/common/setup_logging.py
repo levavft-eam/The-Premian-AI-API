b884979c37c0a9f92cd3cf57fb9fcd8d1e041c55
levavft-eam <levavft@ethan-alice.com>
2024-04-22 14:50:51 +0900

    move logging from main to a new file

---
+++ src/common/setup_logging.py
@@ -0,0 +1,29 @@
+import logging
+import logging.config
+
+import os
+from datetime import datetime
+from dotenv import find_dotenv, load_dotenv
+
+# find .env file in parent directory
+env_file = find_dotenv()
+load_dotenv()
+
+CONFIG_DIR = "../config"
+LOG_DIR = "../logs"
+
+
+def setup_logging():
+    """Load logging configuration"""
+    log_configs = {"dev": "logging.dev.ini", "prod": "logging.prod.ini"}
+    config = log_configs.get(os.environ["ENV"], "logging.dev.ini")
+    config_path = "/".join([CONFIG_DIR, config])
+
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+
+    logging.config.fileConfig(
+        config_path,
+        disable_existing_loggers=False,
+        defaults={"logfilename": f"{LOG_DIR}/{timestamp}.log"},
+    )
+
--- src/main.py
+++ src/main.py
@@ -1,34 +1,7 @@
-# src/main.py
 import logging
-import logging.config
-import os
-from datetime import datetime
 
-from dotenv import find_dotenv, load_dotenv
-
+from common.setup_logging import setup_logging
 from stt.whisper import load_pipeline
-
-# find .env file in parent directory
-env_file = find_dotenv()
-load_dotenv()
-
-CONFIG_DIR = "../config"
-LOG_DIR = "../logs"
-
-
-def setup_logging():
-    """Load logging configuration"""
-    log_configs = {"dev": "logging.dev.ini", "prod": "logging.prod.ini"}
-    config = log_configs.get(os.environ["ENV"], "logging.dev.ini")
-    config_path = "/".join([CONFIG_DIR, config])
-
-    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-
-    logging.config.fileConfig(
-        config_path,
-        disable_existing_loggers=False,
-        defaults={"logfilename": f"{LOG_DIR}/{timestamp}.log"},
-    )
 
 
 if __name__ == "__main__":
