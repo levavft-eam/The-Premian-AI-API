2279e687c972a58df94933ce70adb4f9c8e6b228
levavft-eam <levavft@ethan-alice.com>
2024-04-24 13:25:18 +0900

    add a way to get youtuber metadata

--- src/metadata/google_api.py
+++ src/metadata/google_api.py
@@ -12,6 +12,20 @@ GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
 logger = logging.getLogger(__name__)
 
 
+def get_youtuber_statistics(youtuber_handle):
+    api_service_name = "youtube"
+    api_version = "v3"
+    youtube = build(api_service_name, api_version, developerKey=GOOGLE_API_KEY)
+    request = youtube.channels().list(
+        part="brandingSettings,contentDetails,contentOwnerDetails,id,localizations,snippet,statistics,status,"
+             "topicDetails",
+        forHandle=youtuber_handle
+    )
+
+    response = request.execute()
+    return response
+
+
 def get_video_statistics(video_id):
     api_service_name = "youtube"
     api_version = "v3"
@@ -68,7 +82,9 @@ def _get_video_categories():
 
 
 def test():
-    video_id = "c8OwVTBdE6s"
+    # video_id = "c8OwVTBdE6s"
     # logger.info(pformat(_get_video_categories()))
-    logger.info(pformat(get_video_statistics(video_id)))
+    # logger.info(pformat(get_video_statistics(video_id)))
+    youtuber_handle = "@Coachella"
+    logger.info(pformat(get_youtuber_statistics(youtuber_handle)))
 
