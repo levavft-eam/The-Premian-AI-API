a9a0c0bc435b415beb553c2cc60bb7b5a860122e
levavft-eam <levavft@ethan-alice.com>
2024-04-25 15:58:45 +0900

    add all sorts of functions to google_api.py

--- src/metadata/google_api.py
+++ src/metadata/google_api.py
@@ -16,15 +16,37 @@ api_version = "v3"
 youtube = build(api_service_name, api_version, developerKey=GOOGLE_API_KEY)
 
 
-def get_youtuber_statistics(youtuber_handle):
+def get_playlist_items(playlist_id):
+    return youtube.playlistItems().list(
+        part="snippet,contentDetails,id",
+        playlistId=playlist_id
+    ).execute()
+
+
+def get_channel_playlists(channel_id):
+    return youtube.playlists().list(
+        part="contentDetails, id, snippet",
+        channelId=channel_id,
+        # maxResults=30
+    ).execute()
+
+
+# prob useless
+def get_channel_sections(channel_id):
+    return youtube.channelSections().list(
+        part="id,snippet, contentDetails",
+        channelId=channel_id
+    ).execute()
+
+
+def get_channel_statistics(channel_handle):
     request = youtube.channels().list(
         part="brandingSettings,contentDetails,contentOwnerDetails,id,localizations,snippet,statistics,status,"
              "topicDetails",
-        forHandle=youtuber_handle
+        forHandle=channel_handle
     )
 
-    response = request.execute()
-    return response
+    return request.execute()
 
 
 def get_video_statistics(video_id):
@@ -80,6 +102,15 @@ def test():
     # video_id = "c8OwVTBdE6s"
     # logger.info(pformat(_get_video_categories()))
     # logger.info(pformat(get_video_statistics(video_id)))
-    youtuber_handle = "@Coachella"
-    logger.info(pformat(get_youtuber_statistics(youtuber_handle)))
+
+    # channel_handle = "@Coachella"
+    # logger.info(pformat(get_youtuber_statistics(channel_handle)))
+
+    # channel_id = "UCHF66aWLOxBW4l6VkSrS3cQ"
+    # logger.info(pformat(get_channel_sections(channel_id)))
+    # logger.info(pformat(get_channel_playlists(channel_id)))
+
+    playlist_id = "PLIjqRbAQP0WI1kdiUTD8btfDOGMSZ950U"
+    logger.info(pformat(get_playlist_items(playlist_id)))
+
 
