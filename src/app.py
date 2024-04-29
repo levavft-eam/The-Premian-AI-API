2fe3fdc18118f5fc5b03911e3b4ae2d5afe36ded
levavft-eam <levavft@ethan-alice.com>
2024-04-29 15:37:05 +0900

    reorgenize a bit and add more logging.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,14 @@
-from flask import Flask, jsonify, request  # type: ignore
+import logging
+from flask import Flask, jsonify, request, g, Response  # type: ignore
 from werkzeug.middleware.proxy_fix import ProxyFix  # type: ignore
+
 from src.flows import video_categorization, VIDEO_ID, TRANSCRIPT
+from src.common.setup_logging import setup_logging
+
+
+logger = logging.getLogger(__name__)
+setup_logging()
+
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
@@ -10,6 +18,21 @@ app.wsgi_app = ProxyFix(
     app.wsgi_app, x_for=1, x_proto=0, x_host=0, x_prefix=0
     # app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
 )
+
+
+@app.before_request
+def gather_request_data():
+    g.method = request.method
+    g.url = request.url
+
+
+@app.after_request
+def log_details(response: Response):
+    g.status = response.status
+
+    logger.info(f'method: {g.method}\n url: {g.url}\n status: {g.status}')
+
+    return response
 
 
 # .../test
