a3e8d8bc3a66a1c717448c76100ca49b1536862e
levavft-eam <levavft@ethan-alice.com>
2024-05-03 11:54:53 +0900

    small open_ai stt change

--- src/stt/open_ai.py
+++ src/stt/open_ai.py
@@ -2,11 +2,11 @@ from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
+client = OpenAI()
 
 
 def stt(fpath):
     raise Exception("You are using openAI stt. Please update this code and test it before using it again.")
-    client = OpenAI()
 
     with open(fpath, "rb") as audio_file:
         transcript = client.audio.transcriptions.create(
